b2ffaf6c6fd4be8f1c60bd88ff8e3daaff13d9ce
Reyonaes <reo.elza@gmail.com>
2025-03-26 14:58:46 +0530

    Variable names changed

--- flatten_json.py
+++ flatten_json.py
@@ -83,18 +83,20 @@ def flatten_json_df(df_arg: DataFrame, index: int = 1):
                 current_col = column_name
                 append_str = current_col
                 data_type_str = str(df.schema[current_col].dataType)
+
                 # Renames struct fields in case of duplicate names
                 df_temp = df.withColumnRenamed(column_name, \
                             column_name + "#1") if column_name in data_type_str else df
                 current_col = current_col + "#1" if column_name in data_type_str else current_col
+                df_cols_in_struct = df_temp.select(f"{current_col}.*")
 
-                df_before_expanding = df_temp.select(f"{current_col}.*")
                 # Gets the columns inside a struct fields
-                newly_gen_cols = df_before_expanding.columns
+                newly_gen_cols = df_cols_in_struct.columns
                 begin_index = append_str.rfind('*')
                 end_index = len(append_str)
                 level = append_str[begin_index + 1: end_index]
 
+                # Adds the next level in the nesting
                 next_level = int(level) + 1
                 custom_cols = dict((field, f"{append_str}->{field}*{next_level}")
                                    for field in newly_gen_cols)
